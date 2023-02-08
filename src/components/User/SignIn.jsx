cdd1f5709dca1d470dd3a52e29aab9e5b4d7b91c
InetaKr <ineta.kriunaite@outlook.com>
2023-02-08 09:35:36 +0200

    signIn added

---
+++ src/components/User/SignIn.jsx
@@ -0,0 +1,66 @@
+import UserContext from "../context/UserContext";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const LogIn = () => {
+
+  const {users, setLoggedInUser } = useContext(UserContext);
+  const navigate = useNavigate()
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const [logInInputs, setLogInInputs] = useState({
+    userName: "",
+    password: ""
+  });
+
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const loggedInUser = users.find(
+      user => user.email === logInInputs.email && user.password === logInInputs.password
+    );
+
+    if(loggedInUser){
+      setLoggedInUser(loggedInUser);
+      navigate("/");
+    } else {
+      setErrorMessage("Invalid UserName or password");
+    }
+  };
+
+      return (
+     <>
+     <div className="form-wrapper">
+     <form onSubmit={handleSubmit} className="form">
+      <h1>Log In</h1>
+      <div className="label-wrapper">
+      <label>
+        User Name:
+        <br />
+      <input 
+        type="text"
+        value={logInInputs.userName}
+        onChange= {(e) => setLogInInputs({...logInInputs, userName: e.target.value})
+        }
+      />
+      </label>
+      <label>
+        Password:
+        <br />
+      <input 
+        type="password"
+        value={logInInputs.password}
+        onChange= {(e) => setLogInInputs({...logInInputs, password: e.target.value})
+        }
+      />
+      </label>
+      </div>
+      <button type="submit"> Log In </button>
+      {errorMessage && <p>{errorMessage}</p>}
+     </form>
+     </div>
+     </>
+    );
+  }
+  
+  export default LogIn;
