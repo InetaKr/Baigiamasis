d9248bb08be42daef24dc6d5fb01c5e9546590bf
InetaKr <ineta.kriunaite@outlook.com>
2023-02-12 18:42:17 +0200

    added some info to home and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./styling/App.css";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/PageParts/Header";
 import Home from "./components/PageParts/Home";
 import SignIn from "./components/User/SignIn";
@@ -9,14 +9,12 @@ import AddQuestion from "./components/Questions/AddQuestion";
 import EditQuestion from "./components/Questions/EditQuestion";
 import AnswerPage from "./components/PageParts/AnswerPage";
 import Footer from "./components/PageParts/Footer";
-import Hero from "./components/PageParts/Hero";
+
 
 const App = () => {
-  const location = useLocation();
   return (
     <>
       <Header />
-      {location.pathname === "/" ? <Hero /> : null}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signIn" element={<SignIn />} />
