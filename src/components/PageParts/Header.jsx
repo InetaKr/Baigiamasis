c5fc1e7e58f97787e70a8190241a41fd72ff8695
InetaKr <ineta.kriunaite@outlook.com>
2023-02-12 16:01:19 +0200

    minor fixed header/ hero and styling

--- src/components/PageParts/Header.jsx
+++ src/components/PageParts/Header.jsx
@@ -2,44 +2,47 @@ import { Link, Outlet } from "react-router-dom";
 import { useContext } from "react";
 import UserContext from "../../context/UserContext";
 import UserInfo from "../User/UserInfo";
+import companyLogo from '../../images/logo.png';
 
 const Header = () => {
   const { loggedInUser } = useContext(UserContext);
 
   return (
     <>
-      <nav>
-        {loggedInUser ? (
-          <div className="navBarWrapper">
-            <div className="linkWrapper">
-              <div>
-                <Link to="/">HOME </Link>
-              </div>
-              <div>
-                <Link to="/forum">Forum</Link>
-              </div>
-              <div>
-                <Link to="/add">Ask Question</Link>
-              </div>
+      <nav className="header-nav">
+      <div className="logo">
+          <Link to="/">
+            <img src={companyLogo} alt="" />
+            </Link>  
+            <p>Pawzly</p>
+        </div>
+        <div className="header-nav-wrapper">
+          <div className="header-nav-left">
+            <Link to="/" className="header-nav-link">
+              Home
+            </Link>
+            <Link to="/forum" className="header-nav-link">
+              Forum
+            </Link>
+            <Link to="/add" className="header-nav-link">
+              Ask Question
+            </Link>
+          </div>
+          {loggedInUser ? (
+            <div className="header-nav-right">
+              <UserInfo />
             </div>
-            <UserInfo />
-          </div>
-        ) : (
-          <div className="loginRegister">
-            <Link to="/">HOME </Link>
-            <br />
-            <Link to="/signIn">Sign In</Link>
-            <br />
-
-            <Link to="/signUp">Sign Up</Link>
-
-            <br />
-            <Link to="/forum">Forum</Link>
-
-            <br />
-            <Link to="/add">Ask Question</Link>
-          </div>
-        )}
+          ) : (
+            <div className="header-nav-right">
+              <Link to="/signIn" className="header-nav-link">
+                Sign In
+              </Link>
+              <Link to="/signUp" className="header-nav-link">
+                Sign Up
+              </Link>
+            </div>
+          )}
+        </div>
       </nav>
       <Outlet />
     </>
